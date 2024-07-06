fff597f0508fdb2b46b295e9c88cd89e61275512
syu3ma4syo <syutvlove@gmail.com>
2024-07-06 21:51:02 +0900

    OpenAI APIを呼び出す

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ slackのAIを使ったアプリ
 
 # pip
 pip install slack_bolt==1.18.0 python-dotenv==1.0.0
+pip install langchain==0.0.292 openai==0.28.0
--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+import re
+from langchain.chat_models import ChatOpenAI
 
 load_dotenv()
 
@@ -9,10 +11,16 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 @app.event("app_mention")
 def handle_mention(event, say):
-    user = event["user"]
     thread_ts = event["ts"]
-    say(thread_ts=thread_ts,text=f"こんにちは <@{user}>")
-    say(f"Hello <@{user}>!")
+    message = re.sub("<@.*>","",event["text"])
+
+    llm = ChatOpenAI(
+        model_name=os.environ["OPENAI_API_MODEL"],
+        temperature=os.environ["OPENAI_TEMPERATURE"],
+    )
+
+    response = llm.predict(message)
+    say(thread_ts=thread_ts,text=response)
 
 if __name__ == "__main__" :
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
