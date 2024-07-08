922808c7a8764c396509983aefb060bca79f59ca
syu3ma4syo <syutvlove@gmail.com>
2024-07-08 09:38:36 +0900

    完成

--- app.py
+++ app.py
@@ -58,7 +58,21 @@ class SlackStreamingCallbackHandler(BaseCallbackHandler):
                 self.interval = self.interval * 2
     
     def on_llm_end(self, response: LLMResult, **kwargs: Any)->Any:
-        app.client.chat_update(channel=self.channel, ts=self.ts, text=self.message)
+        message_context = "OpenAI APIで生成される情報は不正確または不適切な場合がありますが、当社の見解を述べるものではありません。"
+        message_blocks = [
+            {"type": "section", "text": {"type": "mrkdwn", "text":self.message}},
+            {"type": "divider"},
+            {
+                "type": "context",
+                "elements": [{"type":"mrkdwn", "text": message_context}],
+            },
+        ]
+        app.client.chat_update(
+            channel=self.channel, 
+            ts=self.ts, 
+            text=self.message,
+            blocks=message_blocks,
+        )
 
 def handle_mention(event, say):
     channel = event["channel"]
